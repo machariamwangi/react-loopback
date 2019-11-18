199f114799cc788c1c1e3e13a70efac2855f69ef
Elimax Mwangi <elimax@localhost.localdomain>
2019-11-18 16:17:48 -0500

    compling and intergrating of frontend and backend

---
+++ precache-manifest.11c534d6f10ac89079377370ddd1b571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8694d8da965a64158a60f27a24be688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef576c5f4e91cfd1ba6",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "26a94da186fc45528e11",
+    "url": "/static/js/2.a37663db.chunk.js"
+  },
+  {
+    "revision": "7ef576c5f4e91cfd1ba6",
+    "url": "/static/js/main.e4c5ebe0.chunk.js"
+  },
+  {
+    "revision": "4b10e7f5113f1f738077",
+    "url": "/static/js/runtime-main.f2ef5ac4.js"
+  }
+]);
